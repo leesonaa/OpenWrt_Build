c2538cad1bca44b8552ae58cbd7b640c202ad88b
leeson <97571961+leesonaa@users.noreply.github.com>
2024-06-16 07:11:50 +0800

    add mt3kdiy.sh

---
+++ mt3kdiy.sh
@@ -0,0 +1,33 @@
+#
+#!/bin/bash
+# © 2022 GitHub, Inc.
+#====================================================================
+# Copyright (c) 2022 Ing
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/wjz304/OpenWrt_Build
+# File name: diy.sh
+# Description: OpenWrt DIY script
+#====================================================================
+
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+
+# Modify maximum connections
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="OpenWrt"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"OpenWrt"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+
